39e691d83d44bbc96db6a68e97c3766a175afcb1
Adel OzZa <adelcasper99@gmail.com>
2026-07-19 01:48:58 +0300

    chore(license): delete dead LicenseActivationScreen and redirect /activate to /onboarding

--- src/app/activate/page.tsx
+++ src/app/activate/page.tsx
@@ -1,11 +1,5 @@
-import LicenseActivationScreen from '@/components/setup/LicenseActivationScreen';
-
-export const metadata = {
-    title: 'تفعيل كاسبر للحوسبة السحابية',
-    description: 'Activate Casper POS Desktop with your Cloud License',
-};
+import { redirect } from 'next/navigation';
 
 export default function ActivatePage() {
-    return <LicenseActivationScreen />;
+    redirect('/onboarding');
 }
-
